bfa59a1c499fbc7ef42401063c6cdd804c863486
Beamlak1234 <beamlakthaymanot@gmail.com>
2024-03-08 15:16:08 +0300

    adding LAB-1-function.dart

---
+++ LAB-1-function-1.dart
@@ -0,0 +1,12 @@
+void main() {
+  double number1 = 5;
+  double number2 = 7;
+
+  double sum = calculateSum(number1, number2);
+
+  print("The sum of $number1 and $number2 is $sum");
+}
+
+double calculateSum(double num1, double num2) {
+  return num1 + num2;
+}
