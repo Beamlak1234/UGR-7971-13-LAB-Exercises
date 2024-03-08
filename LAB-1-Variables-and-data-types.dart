73e7a05de7be59da830e6aae519d9b9c308de0cf
Beamlak1234 <beamlakthaymanot@gmail.com>
2024-03-08 14:33:30 +0300

    adding variable and data types-1

---
+++ LAB-1-Variables-and-data-types.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Beamlak";
+  int age = 3;
+  String favoriteColor = "Blue";
+
+  print("My name is $name.");
+  print("I am $age years old.");
+  print("My favorite color is $favoriteColor.");
+}
