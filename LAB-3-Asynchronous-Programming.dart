f42f277ec3ba76b752a96554f843e7ba89a40ffd
Beamlak T/haymanot <148187123+Beamlak1234@users.noreply.github.com>
2024-03-08 17:00:09 +0300

    Update LAB-3-Asynchronous-Programming.dart

--- LAB-3-Asynchronous-Programming.dart
+++ LAB-3-Asynchronous-Programming.dart
@@ -2,10 +2,10 @@ import 'dart:async';
 
 class Database {
   static Future<List<String>> fetchData() async {
-    // Simulate a delay of 2 seconds
+    
     await Future.delayed(Duration(seconds: 2));
 
-    // Simulated data
+   
     List<String> data = ['John', 'Jane', 'Alice', 'Bob'];
 
     return data;
