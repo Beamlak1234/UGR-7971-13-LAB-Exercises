52a78551b632d8b65a642538c7bb349ed5cddd56
Beamlak1234 <beamlakthaymanot@gmail.com>
2024-03-08 15:09:03 +0300

    adding LAB-1-control-flow-3.dart

---
+++ LAB-1-control-flow-3.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+void main() {
+  print("Simple Calculator");
+  print("Enter the first number:");
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the second number:");
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print("Enter the operation (+, -, *, /):");
+  String operator = stdin.readLineSync()!;
+
+  double result = 0;
+
+  switch (operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+    default:
+      print("Invalid operator");
+      return;
+  }
+
+  print("Result: $result");
+}
